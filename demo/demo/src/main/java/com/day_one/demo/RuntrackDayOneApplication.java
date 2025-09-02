e527181ed09779422c54973d72fc4f552b72623b
samy-belazzoug <samy.belazzoug@laplateforme.io>
2025-09-02 16:04:55 +0200

    Job 1 done (job2 almost done).

---
+++ demo/demo/src/main/java/com/day_one/demo/RuntrackDayOneApplication.java
@@ -0,0 +1,13 @@
+package com.day_one.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuntrackDayOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuntrackDayOneApplication.class, args);
+	}
+
+}
---
+++ demo/demo/src/test/java/com/day_one/demo/RuntrackDayOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.day_one.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuntrackDayOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
