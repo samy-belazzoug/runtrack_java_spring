863392279130bde6c27ff6b64511a6de97871566
samy-belazzoug <samy.belazzoug@laplateforme.io>
2025-09-07 23:30:12 +0200

    Job 4 Done.

--- demo/demo/src/main/java/com/day_one/demo/controller/ViewController.java
+++ demo/demo/src/main/java/com/day_one/demo/controller/ViewController.java
@@ -1,13 +1,18 @@
 package com.day_one.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import java.util.List;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import java.util.Arrays;
 
 @Controller
 public class ViewController {
     
     @GetMapping("/view")
-    public String showView() {
+    public String showView(Model model) {
+        List<String> fruits = Arrays.asList("C", "Python", "Rust");
+        model.addAttribute("fruitsList", fruits);   
         return "view";
     }
 }
