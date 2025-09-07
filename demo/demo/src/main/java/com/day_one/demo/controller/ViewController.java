db0429774599ed472dc8316447e42744cbf3f9db
samy-belazzoug <samy.belazzoug@laplateforme.io>
2025-09-07 23:47:41 +0200

    Job 4 Done.

--- demo/demo/src/main/java/com/day_one/demo/controller/ViewController.java
+++ demo/demo/src/main/java/com/day_one/demo/controller/ViewController.java
@@ -1,6 +1,9 @@
 package com.day_one.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,6 +16,12 @@ public class ViewController {
     public String showView(Model model) {
         List<String> fruits = Arrays.asList("C", "Python", "Rust");
         model.addAttribute("fruitsList", fruits);   
+        return "view";
+    }
+
+    @PostMapping("/welcome")
+    public String handleForm(@RequestParam String username, Model model) {
+        model.addAttribute("message", "Bienvenue, " + username + " !");
         return "view";
     }
 }
